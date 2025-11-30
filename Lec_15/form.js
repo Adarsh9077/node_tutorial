854e4119330d24514c6fe7e161f5cf57f320ce6f
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-12-01 00:56:30 +0530

    Working on Form post method

---
+++ Lec_15/form.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write(
+    `<div className="container" style="height:500px;display:flex;     
+    align-items: center;
+    justify-content: center;">
+    <form action="/submit" className="formId" method="post">
+    <div className="name_input_box">
+    <center>
+    <input type="text" placeholder="Enter your Name" name='name' />
+    </center>
+    </div>
+    <br>
+    <div className="email_input_box">
+    <center>
+    <input type="email" placeholder="Enter your Email" name='email' />
+    </center>
+    </div>
+    <br>
+    <center><button type="submit">Submit</button></center>
+    </form>
+    </div>`
+  );
+});
+
+server.listen(3650);
