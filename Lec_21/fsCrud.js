52d1738e0c2be22f2b229cb7d9f2f1dff6f0bb3a
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-04-15 01:10:01 +0530

    working on read write opration using fs module

--- Lec_21/fsCrud.js
+++ Lec_21/fsCrud.js
@@ -1,11 +1,22 @@
-const fs = require('fs');
+const fs = require("fs");
 
-console.log(process.argv);
+console.log(` ----------------> ${process.argv[2]}`);
 const operation = process.argv[2];
 
-if(operation == 'write'){
-    const fileName = process.argv[3];
-    const content = process.argv[4];
-    const fullFileName = "file/"+fileName+".txt";
-    fs.writeFileSync(fullFileName,content)
+if (operation == "write") {
+  const fileName = process.argv[3];
+  const content = process.argv[4];
+  const fullFileName = "file/" + fileName + ".txt";
+  fs.writeFileSync(fullFileName, content);
+} else if (operation == "read") {
+  const fileName = process.argv[3];
+  // const content = process.argv[4];
+  const fullFileName = "file/" + fileName + ".txt";
+  const fileContent = fs.readFileSync(fullFileName, "utf-8");
+  console.log(fileContent);
 }
+
+
+//! start->  Node JS Tutorial in Hindi #22 Make Simple and Basic Website
+
+
