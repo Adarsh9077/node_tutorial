dd3bcf3eeced1bde5fbae0451260403b1fe7a4d4
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-04-29 00:39:05 +0530

    middleware study use cases

---
+++ Lec_31/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app = express();
+
+function ageCheck(req, res, next) {
+  if (!req.query.age || req.query.age < 18) {
+    res.send("<h4>You are not allowed</h4>");
+  } else {
+    next();
+  }
+}
+
+app.use(ageCheck);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home screen</h1>");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Login screen</h1>");
+});
+
+app.get("/admin", (req, res) => {
+  res.send("<h1>Admin screen</h1>");
+});
+
+app.listen("3200", (req, res) => {
+  console.log("Server was start............. at 3200");
+});
