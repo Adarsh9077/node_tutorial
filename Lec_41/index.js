4c7d8bb274e564a13090b8434f366c87577c18d2
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-05-20 00:26:40 +0530

    Working on staic api

---
+++ Lec_41/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import userData from "./users.json" with { type: "json" };
+const app = express();
+
+app.get("/", (req, res) => {
+  //   console.log(userData);
+  res.send(userData);
+});
+
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  let fillerData = userData.filter((user) => user.id == id);
+  res.send(fillerData);
+});
+
+app.get("/username/:name", (req, res) => {
+  const usernameSearch = req.params.name;
+  let username = userData.filter(
+    (user) => user.name.toLowerCase() == usernameSearch.toLowerCase(),
+  );
+  res.send(username);
+});
+
+app.listen(3299);
+
+//! #42  00:00
